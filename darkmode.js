87d45fb958d3d1caf06395689c24701fef7bdbb4
Pxnjo <alecustu7@gmail.com>
2024-12-17 15:16:46 +0100

    applicato sfondo e esteso dark mode

--- darkmode.js
+++ darkmode.js
@@ -3,11 +3,17 @@ const themeSwitch = document.getElementById("theme_switch")
 
 const enableDarkmode = () => {
     document.body.classList.add("darkmode")
+    document.querySelectorAll('.curriculum').forEach(curriculum => curriculum.classList.add("darkmode"))
+    document.querySelectorAll('.curriculum_profile').forEach(curriculum => curriculum.classList.add("darkmode"))
+    document.querySelectorAll('.contacts').forEach(curriculum => curriculum.classList.add("darkmode"))
     localStorage.setItem("darkmode", "active")
 }
 
 const disableDarkmode = () => {
     document.body.classList.remove("darkmode")
+    document.querySelectorAll('.curriculum').forEach(curriculum => curriculum.classList.remove("darkmode"))
+    document.querySelectorAll('.curriculum_profile').forEach(curriculum => curriculum.classList.remove("darkmode"))
+    document.querySelectorAll('.contacts').forEach(curriculum => curriculum.classList.remove("darkmode"))
     localStorage.setItem("darkmode", null)
 }
 
