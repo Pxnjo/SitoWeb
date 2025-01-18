53439e6d232ed31501c5c72daa862a45bb84a4f6
Pxnjo <alecustu7@gmail.com>
2025-01-18 02:31:45 +0100

    fixato bottone cert

---
+++ button_cert.js
@@ -0,0 +1,9 @@
+function ShowDiv() {
+            var x = document.querySelector(".certificazioni_box");
+            if (x.style.display === "none") {
+              x.style.display = "flex";
+            } else {
+              x.style.display = "none";
+            }
+        }
+    
