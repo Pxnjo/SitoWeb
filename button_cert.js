114b4c02fc6b36812ddb98b4a4fcf9c78abfe5b0
Pxnjo <alecustu7@gmail.com>
2025-01-19 00:31:24 +0100

    Squashed commit of the following:
    commit 8959616df707d18f7fb8855027739c18c6c5ed91
    Author: Pxnjo <alecustu7@gmail.com>
    Date:   Sun Jan 19 00:27:49 2025 +0100
    
        nav bar e altro da finire
    
    commit 6dfa0ac189b454d013e41d5f7fb1bc84eeb61c30
    Author: Pxnjo <alecustu7@gmail.com>
    Date:   Sat Jan 18 03:54:29 2025 +0100
    
        vediamo cosa si puo fare

--- button_cert.js
+++ button_cert.js
@@ -1,9 +1,28 @@
 function ShowDiv() {
-            var x = document.querySelector(".certificazioni_box");
+            var x = document.querySelector(".certificazioni_box_2");
             if (x.style.display === "none") {
               x.style.display = "flex";
             } else {
               x.style.display = "none";
             }
         }
+
+
+function highlightText() {
+  // Rimuovi vecchie evidenziazioni
+  const elements = document.querySelectorAll('h1, h2, h3, p, span, li'); // Cerca nei tag specificati
+  elements.forEach(el => {
+      el.innerHTML = el.textContent; // Rimuove le evidenziazioni precedenti
+  });
+
+  // Ottieni il testo di ricerca
+  const query = document.getElementById('searchInput').value.toLowerCase();
+  if (!query) return; // Se il campo è vuoto, non fare nulla
+
+  // Evidenzia le occorrenze del testo
+  elements.forEach(el => {
+      const regex = new RegExp(`(${query})`, 'gi');
+      el.innerHTML = el.innerHTML.replace(regex, '<span class="highlight">$1</span>');
+  });
+}
     
